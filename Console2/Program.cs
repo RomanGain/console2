e44e6762c2a95278455fa658f8e633cd4730837e
RomanGain <romangajnullin@gmail.com>
2016-08-05 17:06:26 +0300

    05.08.2016

--- Console2/Program.cs
+++ Console2/Program.cs
@@ -13,34 +13,95 @@ namespace Console2
     {
         static void Main(string[] args)
         {
-            TcpListener listner = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000));
-            listner.Start();
-            while (true)
+            //TcpListener listner = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000));
+            //listner.Start();
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("SERVER");
+            //        TcpClient client = listner.AcceptTcpClient();
+
+            //        StreamReader sr = new StreamReader(client.GetStream());
+            //        Console.WriteLine("Client : " + sr.ReadLine());
+
+            //        //StreamWriter sw = new StreamWriter(client.GetStream());
+            //        //sw.AutoFlush = true;
+            //        Console.WriteLine("Server : Hey!");
+            //        //sw.WriteLine("Привет");
+
+            //        //Console.WriteLine("Client : " + sr.ReadLine());
+            //        //Console.WriteLine("Server : End");
+            //        //sw.WriteLine("Пока");
+
+            //        client.Close();
+            //    }
+            //    catch(System.IO.IOException io)
+            //    {
+            //        Console.WriteLine("IOException:\n" + io.ToString());
+            //    }
+            //}
+
+            // Устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // Создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            try
             {
-                try
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Начинаем слушать соединения
+                while (true)
                 {
-                    Console.WriteLine("SERVER");
-                    TcpClient client = listner.AcceptTcpClient();
+                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
 
-                    StreamReader sr = new StreamReader(client.GetStream());
-                    Console.WriteLine("Client : " + sr.ReadLine());
+                    // Программа приостанавливается, ожидая входящее соединение
+                    Socket handler = sListener.Accept();
+                    string data = null;
 
-                    //StreamWriter sw = new StreamWriter(client.GetStream());
-                    //sw.AutoFlush = true;
-                    Console.WriteLine("Server : Hey!");
-                    //sw.WriteLine("Привет");
+                    // Мы дождались клиента, пытающегося с нами соединиться
 
-                    //Console.WriteLine("Client : " + sr.ReadLine());
-                    //Console.WriteLine("Server : End");
-                    //sw.WriteLine("Пока");
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
 
-                    client.Close();
-                }
-                catch(System.IO.IOException io)
-                {
-                    Console.WriteLine("IOException:\n" + io.ToString());
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                    // Показываем данные на консоли
+                    Console.Write("Полученный текст: " + data + "\n\n");
+
+                    // Отправляем ответ клиенту\
+                    string reply = "Спасибо за запрос в " + data.Length.ToString()
+                            + " символов";
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+                    if (data.IndexOf("<TheEnd>") > -1)
+                    {
+                        Console.WriteLine("Сервер завершил соединение с клиентом.");
+                        break;
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+
         }
     }
 }
